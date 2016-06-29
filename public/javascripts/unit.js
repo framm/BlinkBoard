d88a448621cdd899f5f4ac4d0665a235e691024a
Mathias Løkke Madsen <MLM@MLM-MBP15-STIBO.local>
2016-06-29 15:20:38 +0200

    Fixes

--- public/javascripts/unit.js
+++ public/javascripts/unit.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var bbUnit = angular.module('bbUnit', ['firebase', 'ngLodash']);
+var bbUnit = angular.module('bbUnit', ['firebase']);
 
 
 /*=============================================>>>>>
