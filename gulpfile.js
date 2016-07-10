a4aa444a79cfee3e5827521c3281b269db2955f1
Mathias Løkke Madsen <mathias.loekke.madsen@gmail.com>
2016-07-10 16:53:30 +0200

    Gulp changes.

--- gulpfile.js
+++ gulpfile.js
@@ -1,79 +1,53 @@
-'use strict';
-
 // Import modules
+require('dotenv')
+	.config();
 var gulp = require('gulp');
-var exec = require('child_process')
-	.exec;
-var livereload = require('gulp-livereload');
+var nodemon = require('gulp-nodemon');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
 // Paths
-var styling = 'public/sass/';
-var viewerStyling = 'public/viewers/';
-var client = ['public/javascripts/**/*', 'public/templates/**/*', 'public/viewers/**/*', 'public/ui-router/**/*'];
-var server = ['blinkboard.js', '.env', 'viewer_modules/**/*', 'views/**/*'];
+var syncPaths = ['public/**/*', 'views/**/*'];
 
-// Compile sass and livereload
-gulp.task('styling', function () {
-	console.log('Compiling sass and reloading client');
+/*----------- Initial run with watchers -----------*/
+gulp.task('default', function () {
+	console.log('Starting gulp');
 
-	gulp.src(styling + '**/*.scss')
-		.pipe(sass()
-			.on('error', sass.logError))
-		.pipe(gulp.dest(styling))
-		.pipe(livereload());
-});
+	nodemon({
+			script: 'blinkboard.js',
+			ignore: ['package.json', 'bower.json', 'public/**.*'],
+			env: {
+				'NODE_ENV': 'development'
+			}
+		})
+		.on('start', function () {
+			browserSync({
+				proxy: 'http://localhost:' + process.env.PORT
+			});
+		});
 
-// Compile viewer sass and livereload
-gulp.task('viewerStyling', function () {
-	console.log('Compiling viewer sass and reloading client');
+	gulp.watch(syncPaths)
+		.on('change', function (event) {
+			// Get filename and path
+			var file = event.path.match(/[\w-]+\.scss/g);
+			var path = event.path.replace(file, '');
 
-	var viewers = ['currencyviewer', 'rssviewer', 'stockviewer', 'iframeviewer'];
-
-	for (var i = viewers.length - 1; i >= 0; i--) {
-		gulp.src(viewerStyling + viewers[i] + '/' + viewers[i] + '.scss')
-			.pipe(sass()
-				.on('error', sass.logError))
-			.pipe(gulp.dest(viewerStyling + viewers[i]));
-	}
-
-	livereload();
-});
-
-// Reload on client changes
-gulp.task('client', function () {
-	console.log('Reloading client');
-
-	gulp.src(client)
-		.pipe(livereload());
-});
-
-// Reload on client changes
-gulp.task('server', function (cb) {
-	console.log('Reloading server');
-
-	var nodeProcess = exec('node blinkboard');
-
-	nodeProcess.stdout.pipe(process.stdout);
-	nodeProcess.stderr.pipe(process.stderr);
-
-	gulp.src(server)
-		.pipe(livereload());
-});
-
-// Initial run
-gulp.task('default', function (cb) {
-	console.log('Starting server');
-
-	var nodeProcess = exec('node blinkboard');
-
-	nodeProcess.stdout.pipe(process.stdout);
-	nodeProcess.stderr.pipe(process.stderr);
-
-	gulp.watch(styling + '**/*.scss', ['styling']);
-	gulp.watch(viewerStyling + '**/*.scss', ['viewerStyling']);
-	gulp.watch(client, ['client']);
-	gulp.watch(server, ['server']);
-
-	livereload.listen();
+			// If scss file, compile it and sync
+			if (file !== null) {
+				gulp.src(path + file)
+					.pipe(sass()
+						.on('error', sass.logError))
+					.pipe(gulp.dest(path))
+					.pipe(reload({
+						stream: true
+					}));
+			} else {
+				// Just sync
+				gulp.src(event.path)
+					.pipe(reload({
+						stream: true
+					}));
+			}
+		});
 });
